d9b5912420edb6b8a9d1c412bc6d8f40b47a1c87
RK <kumar.adventure@gmail.com>
2013-11-12 19:49:18 +0530

    create migration for pickup and dropoff address and show pick up and dropoff location on map for current ride

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -10,6 +10,8 @@ class BookingsController < ApplicationController
 
   def new
     @booking = Booking.new
+    @pickup_address = @booking.pickup_addresses.build
+    @dropoff_address = @booking.dropoff_addresses.build
     @vehicle_preference = @booking.vehicle_preferences.build
   end
 
@@ -18,10 +20,10 @@ class BookingsController < ApplicationController
     @booking.save
     redirect_to root_url
   end
-  
+
   private
   def booking_params
     params[:booking][:user_id] = current_user.id
-    params.require(:booking).permit(:pickup_address, :dropoff_address, :via_address, :number_of_bags, :number_of_passengers, :booked_hours, :flight_info, :recurrent_type, :pickup_datetime, :return_pickup_datetime, :user_id, :vehicle_preferences_attributes => [:number_of_vehicle, :booking_id, :vehicle_type_id])
+    params.require(:booking).permit(:dropoff_address, :via_address, :number_of_bags, :number_of_passengers, :booked_hours, :flight_info, :recurrent_type, :pickup_datetime, :return_pickup_datetime, :user_id, :vehicle_preferences_attributes => [:number_of_vehicle, :booking_id, :vehicle_type_id], :pickup_addresses_attributes => [:address, :booking_id], :dropoff_addresses_attributes => [:address, :booking_id])
   end
 end
--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -2,6 +2,28 @@ class RidesController < ApplicationController
 
 	def index
     @rides = Booking.where(:user_id => current_user.id).last
+  	@pickup_address = PickupAddress.where(:booking_id => @rides.id).first
+  	@dropoff_address = DropoffAddress.where(:booking_id => @rides.id).first
+  	@hash = Gmaps4rails.build_markers(@pickup_address) do |user, marker|
+		  marker.lat user.latitude
+		  marker.lng user.longitude
+		  marker.infowindow @pickup_address.address
+		  marker.picture({
+		  	"url" => "assets/img/ico/map/ico_location_mark_a.png",
+        "width" =>  36,
+        "height" => 36
+		  	})
+		end
+  	@hash1 = Gmaps4rails.build_markers(@dropoff_address) do |user, marker|
+		  marker.lat user.latitude
+		  marker.lng user.longitude
+		  marker.infowindow @dropoff_address.address
+		  marker.picture({
+		  	"url" => "assets/img/ico/map/ico_location_mark_b.png",
+        "width" =>  36,
+        "height" => 36
+		  	})
+		end
   end
 
 	def cancelled_rides
