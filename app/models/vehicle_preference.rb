d5217302b42f5737b24715595cc439996a6b5cf2
RK <kumar.adventure@gmail.com>
2013-12-06 22:13:31 +0530

    create vehicle_type model

--- app/models/vehicle_preference.rb
+++ app/models/vehicle_preference.rb
@@ -2,5 +2,4 @@ class VehiclePreference < ActiveRecord::Base
   belongs_to :bookings
   attr_accessible :number_of_vehicle, :booking_id, :vehicle_type_id
 
-  VEHICLE_PREFERENCE_TYPE = {	:FourWheeler => 1, :TwoWheeler => 2, :Loading => 3, :Flight => 4}
 end
---
+++ app/models/vehicle_type.rb
@@ -0,0 +1,3 @@
+class VehicleType < ActiveRecord::Base
+  attr_accessible :vehicle_type, :vehicle_rate
+end
